2906956b8cd151a34ac4866e4aafd267aee6cfad
MaksymKoloda <maksim741@ukr.net>
2019-05-14 22:38:18 +0300

    Add project files.

---
+++ NUnitProject1/CodeAcademyTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnitProject1
+{
+    [TestFixture]
+    public class CodeAcademyTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+            options.AddArguments
+            (
+                "--start-maximized",
+                "--disable-extensions",
+                "--disable-notifications",
+                "--disable-application-cache"
+            );
+            _driver = new ChromeDriver(options);
+            _driver.Navigate().GoToUrl("https://www.codeacademy.com/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        [TestCase("catalog", "Catalog | Codecademy")]
+        [TestCase("pricing", "The Easiest Way to Learn to Code | Codecademy")]
+        public void CheckTitle(string elementId, string result)
+        {
+            var catalogLink = _driver.FindElement(By.Id(elementId));
+            catalogLink.Click();
+
+            Assert.AreEqual(result, _driver.Title);
+        }
+    }
+}
